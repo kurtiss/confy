1b49c03b98d067d6eef828dff760a82ed8beee38
alts <stephen@evilrobotstuff.com>
2010-11-15 17:38:25 -0800

    Passes an actual database connection, not PymongoHelper, to Monque, via a wrapper that closes the connection

--- src/confy/providers/monquecfg.py
+++ src/confy/providers/monquecfg.py
@@ -13,13 +13,16 @@ from confy.providers.base import *
 import monque
 import pymongo.connection
 
+class MonqueWrapper(monque.Monque):
+    def __del__(self):
+        self.mongodb.connection.end_request()
 
 class MonqueProvider(InstanceProvider, Provider):
     __abstract__ = True
 
     def construct(self, config):
         database = confy.instance("pymongo.{0[mongodb]}".format(config))
-        return monque.Monque(database)
+        return MonqueWrapper(database.get_db())
 
     def __defaults__(self):
         return dict(
