135c9fb890b73370835dd8844bb86d489982b056
alts <stephen@evilrobotstuff.com>
2010-12-13 12:30:07 -0800

    referring to a nonexistent variable.

--- src/confy/providers/monquecfg.py
+++ src/confy/providers/monquecfg.py
@@ -13,7 +13,7 @@ from confy.providers.base import *
 
 class MonqueProvider(InstanceProvider, Provider):
     __abstract__ = True
-    
+
     @classmethod
     def get_wrapper_cls(cls):
         if not hasattr(cls, '_wrapper_cls'):
@@ -22,13 +22,13 @@ class MonqueProvider(InstanceProvider, Provider):
             class MonqueWrapper(monque.Monque):
                 def __del__(self):
                     self.mongodb.connection.end_request()
-            
+
             cls._wrapper_cls = MonqueWrapper
         return cls._wrapper_cls
 
     def construct(self, config):
         database = confy.instance("pymongo.{0[mongodb]}".format(config))
-        return cls.get_wrapper_cls()(database.get_db())
+        return self.get_wrapper_cls()(database.get_db())
 
     def __defaults__(self):
         return dict(
